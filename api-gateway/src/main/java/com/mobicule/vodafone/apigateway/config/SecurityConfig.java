8fdf05aac930944c73723a86f95f342ee7a43f5b
akshay mandge <akshay.mandge@mobicule.com>
2026-02-25 14:18:38 +0530

    Initial commit

--- api-gateway/src/main/java/com/mobicule/vodafone/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/mobicule/vodafone/apigateway/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.mobicule.vodafone.apigateway.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,25 +10,34 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Value("${jwt.enabled:true}")
+    private boolean jwtEnabled;
+
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
 
-        return http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable());
 
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/eureka/**").permitAll()
-                        .pathMatchers("/api/v1/auth/generateOTP").permitAll()
-                        .pathMatchers("/api/v1/auth/refreshToken").permitAll()
-
-
-                        .anyExchange().authenticated()
-                )
-
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(Customizer.withDefaults())
-                )
-
-                .build();
+        if (jwtEnabled) {
+            // JWT Enabled
+            return http
+                    .authorizeExchange(exchanges -> exchanges
+                            .pathMatchers("/eureka/**").permitAll()
+                            .pathMatchers("/api/v1/auth/generateOTP").permitAll()
+                            .pathMatchers("/api/v1/auth/refreshToken").permitAll()
+                            .anyExchange().authenticated()
+                    )
+                    .oauth2ResourceServer(oauth2 ->
+                            oauth2.jwt(Customizer.withDefaults())
+                    )
+                    .build();
+        } else {
+            // JWT Disabled: all APIs open
+            return http
+                    .authorizeExchange(exchanges -> exchanges
+                            .anyExchange().permitAll()
+                    )
+                    .build();
+        }
     }
 }
